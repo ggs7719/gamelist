b34439201e31578373eeed24112ae3efe7cc0d03
ggs7719 <ggs7719@email.com>
2016-06-17 21:37:36 +0800

    category_not append

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,6 +46,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-  params.require(:event).permit(:name, :description, :status)
+  params.require(:event).permit(:name, :description, :status, :category_id)
   end
 end

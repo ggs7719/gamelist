1360d1db916b55e025f92c36a3b04513c6a7a597
ggs7719 <ggs7719@email.com>
2016-06-17 21:14:57 +0800

    ActiveRecord

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
-  has_many :attendees
+  has_many :attendees, :dependent => :destroy
   validates_presence_of :name
+  belongs_to :category
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
 end
